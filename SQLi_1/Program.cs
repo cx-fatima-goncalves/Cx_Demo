2bb2284816d788dc3ab114215420e4ed819458e9
Fatima Goncalves <124080552+cx-fatima-goncalves@users.noreply.github.com>
2026-04-09 11:00:49 +0100

    Checkmarx Bot - Auto Pull Request

--- SQLi_1/Program.cs
+++ SQLi_1/Program.cs
@@ -38,21 +38,23 @@ namespace SQLi_1
             {
                 using (var conn = new SqlConnection("conn..."))
                 {
-                    var sql = "SELECT * FROM Users WHERE username = '" + username + "' AND pwd = '" + password + "'";
+                    var sql = "SELECT * FROM Users WHERE username = @username AND pwd = @password";
                     using (var cmd = new SqlCommand(sql))
                     {
                         cmd.Connection = conn;
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.Parameters.AddWithValue("@password", password);
                         cmd.ExecuteScalar();
                     }
 
                 }
             }
-            catch  
+            catch
             {
 
                 Console.WriteLine("An error has occurred !!");
             }
-           
+
         }
     }
 }
